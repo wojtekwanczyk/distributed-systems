a56f737c31484ec39a65843e317ab9f1ddc6cf11
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2019-04-01 19:13:42 +0200

    lab3 init

---
+++ lab3/doctor.py
@@ -0,0 +1,17 @@
+import pika
+
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
+    channel.basic_publish(exchange='', routing_key='hello', body='Hello World!')
+    print(" [x] Sent 'Hello World!'")
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
