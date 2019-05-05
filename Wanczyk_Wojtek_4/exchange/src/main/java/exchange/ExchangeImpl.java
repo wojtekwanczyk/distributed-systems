0aeb4161ccb14029ab18426c2ff5f2d7eec84cf0
Wojtek Wanczyk <wojtekwanczyk@gmail.com>
2019-05-05 13:31:22 +0200

    add exchange server implementation

--- Wanczyk_Wojtek_4/exchange/src/main/java/exchange/ExchangeImpl.java
+++ Wanczyk_Wojtek_4/exchange/src/main/java/exchange/ExchangeImpl.java
@@ -1,7 +1,66 @@
 package exchange;
 
+import sr.ExchangeClass;
+import sr.ExchangeClass.ExchangeRequest;
+import sr.ExchangeClass.Currency;
+import sr.ExchangeClass.ExchangeStream;
 import sr.ExchangeGrpc.ExchangeImplBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+import io.grpc.stub.StreamObserver;
+
 
 public class ExchangeImpl extends ExchangeImplBase {
 
+    private HashMap<Currency, Double> rates;
+    private Random rand;
+
+
+    ExchangeImpl(){
+        rates = new HashMap<>();
+        rand = new Random();
+        for(Currency currency : Currency.values()){
+            double rate = rand.nextDouble() * 5 + 0.01; // double from 0.01 to 5.01
+            rates.put(currency, rate);
+        }
+    }
+
+    @Override
+    public void subscribeForExchange(ExchangeRequest request,
+                                     StreamObserver<ExchangeStream> responseObserver) {
+        Currency mainCurrency = request.getMainCurrency();
+        List<Currency> extraCurrencyList = request.getExtraCurrencyList();
+        extraCurrencyList.remove(mainCurrency);
+        int waitTime = 1000;
+
+
+        while(true) {
+
+            // send updates
+            for(Currency currency : extraCurrencyList) {
+                double rate = rates.get(currency);
+                ExchangeStream eStream = ExchangeStream
+                        .newBuilder()
+                        .setCurrency(currency)
+                        .setExchangeRate(rate)
+                        .build();
+                responseObserver.onNext(eStream);
+            }
+
+            // change values and wait
+            try {
+                for(Currency currency : rates.keySet()){
+                    double rate = rates.get(currency);
+                    rate += rand.nextDouble() / 5 - 0.2; // delta (-0.2, 0.2)
+                    rates.put(currency, rate);
+                }
+                Thread.sleep(waitTime);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
--- Wanczyk_Wojtek_4/exchange/src/main/java/exchange/ExchangeServer.java
+++ Wanczyk_Wojtek_4/exchange/src/main/java/exchange/ExchangeServer.java
@@ -1,8 +1,52 @@
 package exchange;
 
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class ExchangeServer {
-    public static void main(String[] args) {
-        System.out.println("ok");
+
+    private static final Logger logger = Logger.getLogger(ExchangeServer.class.getName());
+
+    private int port = 50051;
+    private Server server;
+
+    private void start() throws IOException
+    {
+        server = ServerBuilder.forPort(port)
+                .addService(new ExchangeImpl())
+                .build()
+                .start();
+        logger.info("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                // Use stderr here since the logger may have been reset by its JVM shutdown hook.
+                System.err.println("*** shutting down gRPC server since JVM is shutting down");
+                ExchangeServer.this.stop();
+                System.err.println("*** server shut down");
+            }
+        });
+    }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final ExchangeServer server = new ExchangeServer();
+        server.start();
+        server.blockUntilShutdown();
     }
 
 }
